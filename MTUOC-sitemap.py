f0f9a7db20623381a7f459a3670ea02092272e5a
aoliverg <aoliverg@uoc.edu>
2023-09-04 17:30:33 +0200

    Add files via upload

--- MTUOC-sitemap.py
+++ MTUOC-sitemap.py
@@ -3,8 +3,23 @@ import requests
 import codecs
 import sys
 
+import requests
+
+
+
 URL=sys.argv[1]
 outfile=sys.argv[2]
+
+if not URL.startswith("http"): URL="http://"+URL
+
+response = requests.get(URL)
+
+if response.status_code == 200:
+    print('Web site exists')
+    URL=response.url
+else:
+    print('Web site does not exist') 
+    sys.exit()
 
 sortida=codecs.open(outfile,"w",encoding="utf-8")
 
