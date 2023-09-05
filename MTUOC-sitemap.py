60f510b865ea864fe3712ddc002ae196fde3ee72
aoliverg <aoliverg@uoc.edu>
2023-09-05 16:30:10 +0200

    Add files via upload

--- MTUOC-sitemap.py
+++ MTUOC-sitemap.py
@@ -5,7 +5,31 @@ import sys
 
 import requests
 
+from bs4 import BeautifulSoup
 
+
+def searchGoogle(query):
+
+    headers = {
+        'User-agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
+
+    params = {
+      'q': 'site :'+query,
+      #'gl': 'us',
+      #'hl': 'en',
+    }
+    links=[]
+    html = requests.get('https://www.google.com/search', headers=headers, params=params)
+    soup = BeautifulSoup(html.text, 'lxml')
+
+    for result in soup.select('.tF2Cxc'):
+      #title = result.select_one('.DKV0Md').text
+        link = result.select_one('.yuRUbf a')['href']
+      #print(title, link, sep='\n')
+        links.append(link)
+    return(links)
 
 URL=sys.argv[1]
 outfile=sys.argv[2]
@@ -24,6 +48,19 @@ else:
 sortida=codecs.open(outfile,"w",encoding="utf-8")
 
 tree = sitemap_tree_for_homepage(URL)
+cont=0
 for page in tree.all_pages():
     print(page.url)
     sortida.write(page.url+"\n")
+    cont+=1
+
+if cont==0:
+    cadena="###URL###"
+    sortida.write(cadena+"\n")
+    sortida.write(URL+"\n")
+    
+    cadena="###Google###"
+    sortida.write(cadena+"\n")
+    googlelinks=searchGoogle(URL)
+    for gl in googlelinks:
+        sortida.write(gl+"\n")
